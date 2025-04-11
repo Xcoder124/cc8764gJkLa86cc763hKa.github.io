35346c12c16fbed0cd7a360e93c892d685cc7f6d
Xcoder124 <80002020+Xcoder124@users.noreply.github.com>
2025-04-11 18:51:45 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,18 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDuejeV9uXiLT4IhD-uKvgbT8ZipPkYo34",
-    authDomain: "redemption-page.firebaseapp.com",
-    projectId: "redemption-page",
-    storageBucket: "redemption-page.firebasestorage.app",
-    messagingSenderId: "769888279262",
-    appId: "1:769888279262:web:358ceb060a62062077404f",
-    measurementId: "G-TLFCW9Q4MH"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDuejeV9uXiLT4IhD-uKvgbT8ZipPkYo34",
+  authDomain: "redemption-page.firebaseapp.com",
+  projectId: "redemption-page",
+  storageBucket: "redemption-page.appspot.com",
+  messagingSenderId: "769888279262",
+  appId: "1:769888279262:web:358ceb060a62062077404f",
+  measurementId: "G-TLFCW9Q4MH"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };  // Make db available to other files
